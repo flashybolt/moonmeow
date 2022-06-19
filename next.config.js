eee642088fa52e3442ef6eb559f032b32f9cb157
sc-github-dev <wodaguge369@gmail.com>
2022-06-19 16:17:00 -0400

    redirect / to /profile/

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/profile',
+        permanent: true,
+      },
+    ]
+  },
 }
